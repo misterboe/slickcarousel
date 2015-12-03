4a3574949540d3ed96f0a340cbd7bfde38e45d59
Boris Schauer <me@bschauer.de>
2015-12-03 19:45:34 +0100

    [TASK] Set VHS dependencies version

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'typo3' => '7.6.0-7.99.99',
-            'vhs' => '6.2.0-7.99.99',
+            'vhs' => '^2.4.0',
         ),
         'conflicts' => array(
 
