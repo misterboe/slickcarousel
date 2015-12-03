7287feb89039ebbc513a246c76c700f70aaa9de1
Boris Schauer <me@bschauer.de>
2015-12-03 19:15:48 +0100

    Lets start

---
+++ ext_emconf.php
@@ -0,0 +1,22 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Slick Slider',
+    'description' => 'the last carousel you\'ll ever need',
+    'category' => 'templates',
+    'version' => '0.0.1',
+    'state' => 'beta',
+    'clearcacheonload' => 1,
+    'author' => 'Boris Schauer',
+    'author_email' => 'me@bschauer.de',
+    'author_company' => 'bschauer',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '7.6.0-7.99.99',
+            'vhs' => '6.2.0-7.99.99',
+        ),
+        'conflicts' => array(
+
+        ),
+    ),
+);
