b8a8ac1414d5bfccef372adc6db20a26ef7545a8
Boris Schauer <me@bschauer.de>
2015-12-05 20:19:00 +0100

    [TASK] Create new FAL field for Slider

--- Configuration/TCA/Overrides/CE/slickcarousel.php
+++ Configuration/TCA/Overrides/CE/slickcarousel.php
@@ -1,49 +1,51 @@
 <?php
 
-$tmp_slickslider = array(
-    'tx_slickcarousel' => array(
+$slick_default_fields = array(
+    'slickdefault' => array(
         'exclude' => 0,
-        'label' => 'Slick Carousel Items',
-        'config' => array(
-            'type' => 'inline',
-            'foreign_table' => 'tx_slickcarousel',
-            'foreign_field' => 'tt_content',
-            'maxitems' => 999,
-            'appearance' => array(
-                'collapseAll' => true,
-                'useSortable' => true,
-                'showSynchronizationLink' => false,
-                'showAllLocalizationLink' => true,
-                'showPossibleLocalizationRecords' => false,
-                'showRemovedLocalizationRecords' => false,
-                'expandSingle' => true,
-                'enabledControls' => array(
-                    'localize' => true,
-                ),
+        'label' => 'LLL:EXT:slickcarousel/Resources/Private/Language/be_locallang.xlf:slides',
+        'config' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig(
+            'slickdefault',
+            array(
+                'collapseAll' => 1,
+                'maxitems' => 99,
+                'foreign_types' => array(
+                    '0' => array(
+                        'showitem' => '
+    										--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+    										--palette--;;filePalette'
+                    ),
+                    \TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE => array(
+                        'showitem' => '
+    										--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+    										--palette--;;filePalette'
+                    ),
+                )
             ),
-            'behaviour' => array(
-                'localizationMode' => 'select',
-                'mode' => 'select',
-                'localizeChildrenAtParentLocalization' => true,
-            ),
+            $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext']
         ),
     ),
 );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $tmp_slickslider);
-
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $slick_default_fields);
 
 $GLOBALS['TCA']['tt_content']['types']['slickcarousel'] = array(
-    'showitem' => '	--palette--;Settings;general,
-                    tx_slickcarousel;LLL:EXT:slickcarousel/Resources/Private/Language/be_locallang.xlf:slides,
-					--div--;LLL:EXT:slickcarousel/Resources/Private/Language/be_locallang.xlf:settings,
-					pi_flexform;LLL:EXT:slickcarousel/Resources/Private/Language/be_locallang.xlf:settings,
-					--div--;LLL:EXT:cms/locallang_ttc.xlf:tabs.access,
-					--palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.visibility;visibility,
-					--palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.access;access,
-					--div--;Gridelements,
-					tx_gridelements_container,
-					tx_gridelements_columns,'
+    'showitem' => '
+        --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+		header;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header.ALT.div_formlabel,rowDescription,
+		slickdefault;LLL:EXT:slickcarousel/Resources/Private/Language/be_locallang.xlf:slides,
+		--div--;LLL:EXT:slickcarousel/Resources/Private/Language/be_locallang.xlf:settings,
+		pi_flexform;LLL:EXT:slickcarousel/Resources/Private/Language/be_locallang.xlf:settings,
+	--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+		--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
+	--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access,
+		--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.visibility;visibility,
+		--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+	--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.extended
+	--div--;Gridelements,
+	    tx_gridelements_container,
+        tx_gridelements_columns,
+    '
 );
 
 $GLOBALS['TCA']['tt_content']['columns']['pi_flexform']['config']['ds'][',slickcarousel'] =
