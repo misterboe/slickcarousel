32b6c0cec88398bf6bc20db1d4736640b980e464
SvenJuergens <SvenJuergens@users.noreply.github.com>
2020-06-19 10:37:04 +0200

    [BUGFIX] remove call to addTCAcolumns width undefined variable

--- Configuration/TCA/Overrides/sys_file_reference.php
+++ Configuration/TCA/Overrides/sys_file_reference.php
@@ -4,8 +4,6 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('sys_file_reference', $tmp_sys_file_reference);
-
 $GLOBALS['TCA']['sys_file_reference']['palettes']['slickslider'] = [
     'showitem' => 'title,alternative,--linebreak--,crop,description'
 ];
